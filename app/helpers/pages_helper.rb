225f815d6589ba5833b35f97c500d879e6754294
Sam Serpoosh <ssjesus287@gmail.com>
2012-05-01 13:06:21 +0430

    Completed the layout structure

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,13 @@
 module PagesHelper
+
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App | "
+    if @title.nil?
+      base_title += "Default"
+    else
+      base_title += @title
+    end
+    base_title
+  end
+
 end
