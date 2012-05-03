37f3d8e78dcfefce864b0322b13fce62970d68b9
Sam Serpoosh <ssjesus287@gmail.com>
2012-05-03 18:38:41 +0430

    added layout links with routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App | "
+    if @title.nil?
+      base_title += "Default"
+    else
+      base_title += @title
+    end
+    base_title
+  end
+
+
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,13 +1,2 @@
-module PagesHelper
-
-  def title
-    base_title = "Ruby on Rails Tutorial Sample App | "
-    if @title.nil?
-      base_title += "Default"
-    else
-      base_title += @title
-    end
-    base_title
-  end
-
+module PagesHelper 
 end
