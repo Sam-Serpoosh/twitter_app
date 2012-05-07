ebf7448ad6b8c23d06f75db923006faac39a82c4
Sam Serpoosh <ssjesus287@gmail.com>
2012-05-08 02:45:19 +0430

    added User Factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+Factory.define :user do |user|
+  
+  user.name "Masih Jesus"
+  user.email "ssjesus287@gmail.com"
+  user.password "foobar"
+  user.password_confirmation "foobar"
+
+end #when the tests run this will automatically create this user into the database
