3b34f37ddd132a640c4d6821a8153362d082f948
Sam Serpoosh <ssjesus287@gmail.com>
2012-05-12 04:38:55 +0430

    Finished User sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,15 @@ class UsersController < ApplicationController
     @title = "Sign up"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Twitter App!"
+      redirect_to user_path(@user)
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,7 +18,7 @@ describe UsersController do
 
     it "should find the right user" do
       get :show, :id => @user
-      assigns(:user).should == @user
+      assigns(:user).should == @user #assigns will pull out the instance variable @user from the controller
     end
 
     it "should have the right title" do
@@ -54,6 +54,60 @@ describe UsersController do
     it "should have the right title" do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
+    end
+
+  end
+
+  describe "POST 'create'" do
+
+    describe "failure" do
+
+      before do
+        @attr = { :name => "", :email => "", :password => "", 
+                  :password_confirmation => "" }
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+    end
+
+    describe "success" do
+
+      before do
+        @attr = { :name => "New User", :email => "user@example.com", 
+                  :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the twitter app!/i
+      end
+
     end
 
   end
